d3aff8f69635c5be41c577162c051f85e4bc2c73
Adrián Callejo <callejo@pumpunhost.com>
2023-08-29 19:47:55 +0200

    Added icon and name and splash page created

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'generated/l10n.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,10 +13,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'wallafind',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xff13c1ac)),
         useMaterial3: true,
       ),
+      localizationsDelegates: const [
+        AppLocalizationDelegate(),
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale("es", ''),
+        Locale("en", ''),
+      ],
       home: const Scaffold(),
     );
   }
